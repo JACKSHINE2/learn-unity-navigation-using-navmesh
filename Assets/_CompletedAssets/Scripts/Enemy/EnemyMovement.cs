8a3460fc0e0c4c0b32a3a2bcb291376a2b651710
Rathod Dhaval <rdgrajput@gmail.com>
2017-03-24 09:49:23 +0530

    no message

---
+++ Assets/_CompletedAssets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+	Transform player;
+	// Reference to the player's position.
+	NavMeshAgent nav;
+	// Reference to the nav mesh agent.
+
+	void Awake ()
+	{
+		// Set up the references.
+		player = GameObject.FindGameObjectWithTag ("Player").transform; 
+
+//			Do not use FindGameObjectWithTag() method if the scene hierarchy is too big. 
+//			In such case, declare 'player' as a public variable & set reference from the inspector.
+
+		nav = GetComponent <NavMeshAgent> ();
+
+	}
+
+	void Update ()
+	{
+		// Set the destination of the nav mesh agent to the player.
+		nav.SetDestination (player.position);
+	}
+}
